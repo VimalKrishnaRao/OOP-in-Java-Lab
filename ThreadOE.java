5e523abde425cab898ca0e19a3aaf3909efc59a4
VimalKrishnaRao <96693740+VimalKrishnaRao@users.noreply.github.com>
2022-12-15 17:35:57 +0530

    Create ThreadOE.java

---
+++ ThreadOE.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+class RandomNumberThread extends Thread 
+{
+	public void run() 
+	{
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) 
+		{
+			int randomInteger = random.nextInt(100);
+			System.out.println("Random Integer generated : " + randomInteger);
+			if((randomInteger%2) == 0) 
+			{
+				Even eThread = new Even (randomInteger);
+				eThread.start();
+			}
+			else 
+			{
+				Odd oThread = new Odd (randomInteger);
+				oThread.start();
+			}
+			try 
+			{
+				Thread.sleep(1000);
+			} 
+			catch (InterruptedException e) 
+			{
+				System.out.println(e);
+			}
+		}
+	}
+}
+
+class Even extends Thread 
+{
+	int number;
+
+	Even (int randomNumbern) 
+	{
+		number = randomNumbern;
+	}
+    public void run() 
+    {
+        for (int i=0; i<number;i++)
+        if (i%2==0)
+		    System.out.println(i);
+	}
+}
+
+class Odd extends Thread 
+{
+	int number;
+
+	Odd (int randomNumber) 
+	{
+		number = randomNumber;
+	}
+    public void run() 
+	{
+		for (int i=0; i<number;i++)
+        if (i%2!=0)
+		    System.out.println(i);
+	}
+}
+
+public class MultiThreadingTest 
+{
+	public static void main(String args[]) 
+	{
+		RandomNumberThread rnThread = new RandomNumberThread();
+		rnThread.start();
+	}
+}
