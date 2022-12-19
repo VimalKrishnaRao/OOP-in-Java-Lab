14fa54d25816817460aaa75a6272a37e5021ddc2
VimalKrishnaRao <96693740+VimalKrishnaRao@users.noreply.github.com>
2022-12-19 06:13:18 +0530

    Update Larmat.java

--- Larmat.java
+++ Larmat.java
@@ -1,27 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 class Larmat
 {
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		int a[][]=new int[5][5];
-		int lar;
-		System.out.println("Enter the Matrix Size:");
-		int n=sc.nextInt();
-		System.out.println("Enter the Matrix:");
-		for (int i=0;i<n;i++)
-		{
-			for (int j=0;j<n;j++)
-			{
-				a[i][j]=sc.nextInt();
-			}
-		}
-		lar=a[0][0];
-		for (int i=0;i<n;i++)
-		{
-				if (a[i][j]>lar)
-					lar=a[i][j];
-		}
-		System.out.println("Tha largest element in the matrix is:"+lar);
-	}
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println ("Enter the Matrix Size: ");
+        int n = sc.nextInt();
+        int a[][]=new int [n][n];
+        System.out.println ("Enter an "+n+"X"+n+" Matrix:");
+            for (int i=0; i<n; i++)
+                for (int j=0; j<n; j++)
+                    a[i][j]=sc.nextInt();
+        int lar = a[0][0];
+        for (int i=0; i<n; i++)
+            for (int j=0; j<n; j++)
+                if (lar<a[i][j])
+                    lar = a[i][j];
+        System.out.println ("The Largest Element in the Matrix is: "+lar);
+    }
 }
