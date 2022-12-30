795b650cdc96e17649ca5ff4968e7418e0641ea4
VimalKrishnaRao <96693740+VimalKrishnaRao@users.noreply.github.com>
2022-12-30 15:47:48 +0530

    Update CharFrequency.java

--- CharFrequency.java
+++ CharFrequency.java
@@ -1,21 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
+
 class CharFrequency
 {
-	public static void main(String args[])
-	{
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter the String:");
-		String str=sc.nextLine();
-		System.out.println("Enter the character:");
-		char c=sc.nextLine().charAt(0);
-		int count=0;
-		for (int i=0;i<str.length();i++)
-		{
-			if (c==str.charAt(i))
-			{
-				count++;
-			}
-		}		
-		System.out.println("Frequency of "+c+" in "+str+" is "+count);
-	}
+  public static void main(String args[]) 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a string:");
+    String str = sc.nextLine();
+    System.out.println("Enter a character whose frequemcy has to be found:");
+    char c = sc.next().charAt(0);
+    str = str.toLowerCase();
+    c = Character.toLowerCase(c);
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) 
+    {
+      if (str.charAt(i) == c) 
+      {
+        count++;
+      }
+    }
+    System.out.println("Frequency of character '" + c + "' in string: " + count);
+  }
 }
