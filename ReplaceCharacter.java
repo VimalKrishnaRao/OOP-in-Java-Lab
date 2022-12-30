bfc3d6338114d0c212a04260e86b2daee2bba082
VimalKrishnaRao <96693740+VimalKrishnaRao@users.noreply.github.com>
2022-12-30 15:37:36 +0530

    Update ReplaceCharacter.java

--- ReplaceCharacter.java
+++ ReplaceCharacter.java
@@ -1,16 +1,31 @@
-import java.util.*;
-public class ReplaceCharacter 
+import java.util.Scanner;
+
+class ReplaceCharacter
 {
-   public static void main(String args[])
-   {
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter a string value:");
-      String str = sc.nextLine();
-      System.out.print("Enter the character to be replaced:");
-      char character = sc.nextLine().charAt(0);
-      System.out.print("Enter the Character with which replacement has to be made:");
-      char replacechar = sc.nextLine().charAt(0);
-      String new_str=str.replace(character,replacechar);
-      System.out.println("Final String is "+new_str);
-   }
+  public static void main(String args[]) 
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a string:");
+    String str = sc.nextLine();
+    System.out.println("Enter a character to replace:");
+    char c1 = sc.next().charAt(0);
+    System.out.println("Enter the replacement character:");
+    char c2 = sc.next().charAt(0);
+    str = str.toLowerCase();
+    c1 = Character.toLowerCase(c1);
+    c2 = Character.toLowerCase(c2);
+    String result = "";
+    for (int i = 0; i < str.length(); i++) 
+    {
+      if (str.charAt(i) == c1) 
+      {
+        result += c2;
+      } 
+      else 
+      {
+        result += str.charAt(i);
+      }
+    }
+    System.out.println("Modified string: " + result);
+  }
 }
